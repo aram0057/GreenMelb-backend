9e3771028b69b0b3a4a8b7d1c5d37fe2010a91a7
CheeMinHao <cheemh3456@gmail.com>
2024-08-31 19:50:03 +1000

    update api routing

--- api/urls.py
+++ api/urls.py
@@ -15,16 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from django.contrib import admin  
-from django.views.generic import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('classification/', include('classification.urls')),
+    path('maps/', include('maps.urls')),  # Include URLs from the classification app
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
